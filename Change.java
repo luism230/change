97f9125187523b2a5474877b97c965c6976b539e
Luis Ramirez Mendoza <luism.ramirez23@gmail.com>
2020-09-03 14:33:04 -0700

    number of ways to make change

--- Change.java
+++ Change.java
@@ -1,5 +1,40 @@
 public class Change {
 	public static void main(String[] args){
 		System.out.println("hello world");
+		System.out.println(NumberOfWaysToMakeChange(100, 0) + "combos");
+
+	}
+	public static int NumberOfWaysToMakeChange (int cents, int start)
+	{
+		int number_of_ways = 0;
+		if(cents == 0)
+		{
+			System.out.println("1");
+			return 1 ;
+		}
+		else if(cents > 0)
+		{
+			int[] coins = {25, 10, 5, 1 };
+			
+			for (int i = start; i < coins.length; i++)
+			{
+				System.out.println(cents + "cents");
+				System.out.println(coins[i] + "coins");
+				if(i ==3)
+				{
+					number_of_ways++;
+				}
+				else
+				{
+					number_of_ways += NumberOfWaysToMakeChange(cents - coins[i], i);
+				}
+			}
+		}
+		else if (cents < 0)
+		{
+			System.out.println("0");
+			return 0;
+		}
+		return number_of_ways;
 	}
 }
